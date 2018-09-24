1e7ac6884e8477da8862b173902381abc8a47455
Iván Chicano <ivchicano@gmail.com>
2018-09-24 11:15:20 +0200

    Initial commit

---
+++ app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/MenuActivity.java
@@ -0,0 +1,13 @@
+package procesos.grp7.spaceinvadersprocesossoftware;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
