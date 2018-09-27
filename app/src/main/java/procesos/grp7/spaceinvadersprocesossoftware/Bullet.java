bcd3d6a92a687269edd6e306aa96e91daac64969
ChaosMillenium <darkrai.ivan@gmail.com>
2018-09-27 16:55:05 +0200

    Intento de hacer que la bala se mueva visualmente: -Ya no se guardan las balas activas.
    Problemas:
    -A veces desaparecen instantaneamente las balas.
    -Las balas aparecen donde estaba la nave originalmente,
    ignorando la posición actual.
    -Rezad al señor que no crashee.

--- app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/Bullet.java
+++ app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/Bullet.java
@@ -1,37 +1,61 @@
 package procesos.grp7.spaceinvadersprocesossoftware;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Point;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-public class Bullet {
+public class Bullet extends Thread {
 
-    private Context context;
+    private Activity context;
     private RelativeLayout gameLayout;
     private ImageView bulletView;
     public static final int UP = -1;
     public static final int DOWN = 1;
     private int direction; //-1 hacia arriba, 1 hacia abajo
+    private final int SLEEPTIME = 2;
+    private final String ERROR_LOG = "BULLET_ERROR";
+    private Point screenSize;
 
-    public Bullet(Context context, RelativeLayout gameLayout, int direction) {
+    public Bullet(Activity context, RelativeLayout gameLayout, int direction, Point screenSize) {
         this.context = context;
         this.gameLayout = gameLayout;
         this.direction = direction;
-        this.generateView();
+        this.screenSize = screenSize;
     }
 
-    private void generateView() {
+    public void generateView() {
         ImageView bulletView = new ImageView(context);
         bulletView.setImageResource(R.drawable.bullet);
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         params.addRule(RelativeLayout.ABOVE, R.id.ship);
-        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
         gameLayout.addView(bulletView, params);
         this.bulletView = bulletView;
-
     }
 
     public void update() {
         bulletView.setY(bulletView.getY() + direction);
+    }
+
+    @Override
+    public void run() {
+        int maxY = screenSize.y;
+        try {
+            while ((maxY >= bulletView.getY()) && (bulletView.getY() >= 0)) {
+                Thread.sleep(SLEEPTIME);
+                update();
+            }
+            context.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    gameLayout.removeView(bulletView);
+                }
+            });
+        } catch (InterruptedException e) {
+            Log.e(ERROR_LOG, e.getMessage());
+        }
     }
 }
--- app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/GameActivity.java
+++ app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/GameActivity.java
@@ -1,7 +1,9 @@
 package procesos.grp7.spaceinvadersprocesossoftware;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -11,7 +13,7 @@ import java.util.ArrayList;
 public class GameActivity extends AppCompatActivity {
     private ImageView spriteShip;
     private RelativeLayout gameLayout;
-    private ArrayList<Bullet> bullets = new ArrayList<>();
+    private Point screenSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,8 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         spriteShip = findViewById(R.id.ship);
         gameLayout = findViewById(R.id.layout_game);
+        screenSize = new Point();
+        getWindowManager().getDefaultDisplay().getSize(screenSize);
     }
 
     public void mueveIzquierda(View view) {
@@ -30,8 +34,14 @@ public class GameActivity extends AppCompatActivity {
         float desplazamiento = spriteShip.getX() + 10;
         spriteShip.setX(desplazamiento);
     }
-    public void disparar(View view){
-        Bullet bullet = new Bullet(this, gameLayout, Bullet.UP);
-        bullets.add(bullet);
+
+    public void disparar(View view) {
+        try {
+            Bullet bullet = new Bullet(this, gameLayout, Bullet.UP, screenSize);
+            bullet.generateView();
+            bullet.start();
+        } catch (IllegalStateException e) {
+            Log.w("ILLEGAL_STATE",e.getMessage());
+        }
     }
 }
