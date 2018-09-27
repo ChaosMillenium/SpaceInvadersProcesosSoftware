12eb09c9f1577e4cc6ffc9c088f1eb6956843e21
JesusBorrero <shyfu98@gmail.com>
2018-09-27 12:13:01 +0200

    Incluido el movimiento de un marcianito

--- app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/GameActivity.java
+++ app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/GameActivity.java
@@ -2,25 +2,34 @@ package procesos.grp7.spaceinvadersprocesossoftware;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
-    ImageView sprite;
+    ImageView spriteNave;
+    ImageView spriteMarciano;
+    DisplayMetrics metrics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        sprite = findViewById(R.id.nave);
+        spriteNave = findViewById(R.id.nave);
+        spriteMarciano = findViewById(R.id.marcianito);
+        metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        Marciano marciano = new Marciano(spriteMarciano, metrics.widthPixels, metrics.heightPixels);
+        marciano.start();
     }
 
     public void mueveIzquierda(View view) {
-        float desplazamiento = sprite.getX() - 10;
-        sprite.setX(desplazamiento);
+        float desplazamiento = spriteNave.getX() - 10;
+        spriteNave.setX(desplazamiento);
     }
 
     public void mueveDerecha(View view) {
-        float desplazamiento = sprite.getX() + 10;
-        sprite.setX(desplazamiento);
+        float desplazamiento = spriteNave.getX() + 10;
+        spriteNave.setX(desplazamiento);
     }
 }
