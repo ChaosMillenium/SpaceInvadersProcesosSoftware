1b2f165f4d6c12650b4a04c82e855d7090c2531f
ChaosMillenium <darkrai.ivan@gmail.com>
2018-09-29 16:00:52 +0200

    Añadido método para detectar colisiones.

--- app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/Bullet.java
+++ app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/Bullet.java
@@ -1,8 +1,12 @@
 package procesos.grp7.spaceinvadersprocesossoftware;
 
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.graphics.Point;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -16,6 +20,8 @@ public class Bullet{
     private float direction; //-1 hacia arriba, 1 hacia abajo
     private final int DURATION = 1000;
     private Point screenSize;
+    private ImageView bulletView;
+    private float yPosition;
 
     public Bullet(Activity context, RelativeLayout gameLayout, float direction) {
         this.context = context;
@@ -37,5 +43,27 @@ public class Bullet{
         bulletAnimator.setDuration(DURATION);
         bulletAnimator.setInterpolator(new LinearInterpolator());
         bulletAnimator.start();
+        bulletAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                yPosition = (Float)animation.getAnimatedValue();
+            }
+        });
+    }
+
+    @Nullable
+    public View detectCollision(Iterable<View> views){
+        Rect bullet = new Rect(bulletView.getLeft(),bulletView.getTop(),bulletView.getRight(),bulletView.getBottom());
+        for (View view : views){
+            Rect viewRect = new Rect(view.getLeft(),view.getTop(),view.getRight(),view.getBottom());
+            if (bullet.intersect(viewRect)){
+                return view;
+            }
+        }
+        return null;
+    }
+
+    public synchronized boolean isInScreen(){
+        return (yPosition > 0) && (yPosition<screenSize.y);
     }
 }
