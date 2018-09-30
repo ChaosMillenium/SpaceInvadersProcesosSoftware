51455dd5c71327fb8f3b9cfefb847330cdc8b238
JesusBorrero <shyfu98@gmail.com>
2018-09-30 16:45:06 +0200

    Incluido el movimiento del bloque de marcianitos

--- app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/GameActivity.java
+++ app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/GameActivity.java
@@ -1,26 +1,30 @@
 package procesos.grp7.spaceinvadersprocesossoftware;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class GameActivity extends AppCompatActivity {
     ImageView spriteNave;
-    ImageView spriteMarciano;
-    DisplayMetrics metrics;
+    Display display ;
+    Point size;
+    RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         spriteNave = findViewById(R.id.nave);
-        spriteMarciano = findViewById(R.id.marcianito);
-        metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        Marciano marciano = new Marciano(spriteMarciano, metrics.widthPixels, metrics.heightPixels);
-        marciano.start();
+        display = getWindowManager().getDefaultDisplay();
+        layout = findViewById(R.id.game_layout);
+        size = new Point();
+        display.getSize(size);
+        VistaInvader marcianitos= new VistaInvader(this, size.x, size.y, layout);
+        marcianitos.start();
     }
 
     public void mueveIzquierda(View view) {
@@ -32,4 +36,5 @@ public class GameActivity extends AppCompatActivity {
         float desplazamiento = spriteNave.getX() + 10;
         spriteNave.setX(desplazamiento);
     }
+
 }
--- app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/Marciano.java
+++ app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/Marciano.java
@@ -1,68 +1,84 @@
 package procesos.grp7.spaceinvadersprocesossoftware;
 
-import android.util.Log;
+import android.content.Context;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class Marciano extends Thread{
-    private ImageView spriteMarciano;
-    private int width ;
+public class Marciano {
+    private /*Bitmap*/ImageView spriteMarciano;
+    private int length ;
     private int height ;
-    private String orientacion;
     private float x;
     private float y;
+    private boolean isVisible;
+    private Context context;
+    private String orientacion;
+    private RelativeLayout layout;
 
-    public Marciano (ImageView spriteMarciano, int width, int height){
-        this.spriteMarciano = spriteMarciano;
-        this.spriteMarciano.setY(0);
-        this.spriteMarciano.setX(0);
-        this.x = this.spriteMarciano.getX();
-        this.y = this.spriteMarciano.getY();
-        this.width = width;
-        this.height = height;
-        this.orientacion = "DER";
+    public Marciano (Context context, int screenX, int screenY, int row, int column){
+        this.context = context ;
+        this.length = screenX / 20 ;
+        this.height = screenY / 20 ;
+        this.isVisible = true ;
+        //this.spriteMarciano = BitmapFactory.decodeResource(context.getResources(), R.drawable.spritemarciano) ;
+        //this.spriteMarciano = Bitmap.createScaledBitmap(spriteMarciano, length, height, false) ;
+        //this.spriteMarciano = new ImageView(this.context);
+        this.x = column * (length + (screenX / 25 )) ;
+        this.y = row * (length + (screenX / 25 )/4) ;
+        this.orientacion = "RIGHT";
     }
 
-    private void actualizaPosicion(){
-        if(this.orientacion.equals("DER")) {
-            this.x+=10;
-            this.spriteMarciano.setX(this.x);
-            this.spriteMarciano.setY(this.y);
+    public void actualizaPosicion(){
+        if(this.orientacion.equals("RIGHT")) {
+            this.x += 10;
         }
         else{
             this.x-=10;
-            this.spriteMarciano.setX(this.x);
-           this.spriteMarciano.setY(this.y);
         }
     }
 
-    public void run(){
-        try {
-            while (true) {
-                if(this.orientacion.equals("DER")){
-                    Log.d("width", "" + this.width);
-                    if(this.x < this.width){
-                        Thread.sleep(20);
-                        actualizaPosicion();
-                    }
-                   else{
-                        this.y += 50;
-                        this.orientacion = "IZQ";
-                    }
-                }
-                else{
-                    if(this.x > 0){
-                        Thread.sleep(20);
-                        actualizaPosicion();
-                    }
-                    else{
-                        this.y += 50;
-                        this.orientacion = "DER";
-                    }
-                }
-            }
-        }catch (InterruptedException e){
-            System.out.println(e.getCause());
+    public void choquePared(){
+        if(orientacion.equals("RIGHT")){
+            this.orientacion = "LEFT";
         }
+        else{
+            this.orientacion = "RIGHT";
+        }
+        this.y += 20;
     }
 
+    public void dibuja(){
+        this.spriteMarciano.setX(this.x);
+        this.spriteMarciano.setY(this.y);
+    }
+
+    public void addImageView(RelativeLayout layout){
+        ImageView imageView = new ImageView(context);
+        imageView.setImageResource(R.drawable.spritemarciano);
+        this.spriteMarciano = imageView;
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.height = 40;
+        params.width = 80;
+        layout.addView(imageView, params);
+    }
+
+    public boolean vivo(){
+        return this.isVisible;
+    }
+
+    public float getX(){
+        return this.x;
+    }
+
+    public int getLength(){
+        return this.length;
+    }
+
+    public ImageView getSpriteMarciano() {
+        return spriteMarciano;
+    }
+
+    public float getY() {
+        return y;
+    }
 }
