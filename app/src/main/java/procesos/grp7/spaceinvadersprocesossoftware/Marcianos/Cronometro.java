71d3606e98971ee7637e690290646356df550ab5
JesusBorrero <shyfu98@gmail.com>
2018-10-24 22:51:15 +0200

    Incluido Marciano Loco

---
+++ app/src/main/java/procesos/grp7/spaceinvadersprocesossoftware/Marcianos/Cronometro.java
@@ -0,0 +1,20 @@
+package procesos.grp7.spaceinvadersprocesossoftware.Marcianos;
+
+public class Cronometro extends Thread {
+    private VistaMarcianoEspecial vistaMarcianoEspecial;
+
+    public Cronometro(VistaMarcianoEspecial v){
+        this.vistaMarcianoEspecial = v;
+    }
+
+    public void run(){
+        while (true){
+            try {
+                Thread.sleep(10000);
+                this.vistaMarcianoEspecial.setTemp(true);
+            }catch (InterruptedException e){
+                e.getCause();
+            }
+        }
+    }
+}
